f3892d1fb813bc3ac7955f909e0808d3296ef1f7
Shruti-2303 <sharmas23032001@gmail.com>
2024-01-13 01:06:04 +0530

    Fix: VideoTitle Button Icons

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import VideoTitle from './VideoTitle';
-import VideoBackground from './VideoBackground';
+import React from "react";
+import { useSelector } from "react-redux";
+import VideoTitle from "./VideoTitle";
+import VideoBackground from "./VideoBackground";
 
 const MainContainer = () => {
-    const movies = useSelector((store)=>store.movies?.nowPlayingMovies)
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
 
-    // early return
-    if(!movies) return;
+  // early return
+  if (!movies) return;
 
-    const mainMovie = movies[0];
-    console.log(mainMovie);
+  const mainMovie = movies[0];
+  console.log(mainMovie);
 
-    const {original_title,overview,id} = mainMovie;
-    return (
-        <div className='pt-[30%] bg-black md:pt-0'>
-            <VideoTitle title={original_title} overview={overview}/>
-            <VideoBackground movieId={id}/>
-        </div>
-        
-    )
-}
+  const { original_title, overview, id } = mainMovie;
+  return (
+    <div className="pt-[30%] bg-black md:pt-0">
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
+    </div>
+  );
+};
 
-export default MainContainer
+export default MainContainer;
